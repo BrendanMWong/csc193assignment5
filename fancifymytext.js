458ccf4de673c92d11ebc70961c1ca45e9005de2
Brendan <tobrendanw@gmail.com>
2025-02-17 11:20:28 -0800

    finish assignment 5

--- fancifymytext.js
+++ fancifymytext.js
@@ -16,3 +16,19 @@ function unstyleText() {
     document.getElementById("user-text-area").style.color = "black";
     document.getElementById("user-text-area").style.textDecorationLine = "none";
 }
+
+function moo() {
+    alert("You mooed");
+    let input = document.getElementById("user-text-area");
+    input.value = input.value.toUpperCase();
+    let parts = input.value.split(".");
+
+    for (let count = 0; count < parts.length; count++) {
+        parts[count] = parts[count].trim();
+        if (parts[count].length > 0) {
+            parts[count] = parts[count] + " -Moo";
+        }
+    }
+
+    input.value = parts.join(". ").trim();
+}
